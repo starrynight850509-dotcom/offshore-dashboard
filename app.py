0651ab3e2cee724fc338d720409239b63b5a8b4c
Ali Chang <ali.chang@iovtec.com.tw>
2026-06-10 10:47:16 +0800

    update dashboard

--- app.py
+++ app.py
@@ -10,84 +10,149 @@ import plotly.express as px
 from dash import Dash, dcc, html, Input, Output
 from datetime import datetime
 from dash import get_asset_url
-today = datetime.today().date()
 
-beex_start = datetime(2026,4,6).date()
-iog_start  = datetime(2026,4,18).date()
 
-beex_days = (today - beex_start).days
-iog_days = (today - iog_start).days
-
-summary_table = html.Table([
-    html.Tr([
-        html.Th(""),
-        html.Th("Start"),
-        html.Th("Today"),
-        html.Th("Cumulative Days")
-    ]),
-    html.Tr([
-        html.Td(
-            html.Img(
-                src="/assets/beex_logo.png",
-                style={"height":"40px"}
-            )
-        ),
-        html.Td("2026/4/6"),
-        html.Td(str(today)),
-        html.Td(
-            str(beex_days),
-            style={
-                "color":"red",
-                "fontSize":"30px",
-                "fontWeight":"bold"
-            }
-        )
-    ]),
-    html.Tr([
-        html.Td(
-            html.Img(
-                src="/assets/iog_logo.png",
-                style={"height":"40px"}
-            )
-        ),
-        html.Td("2026/4/18"),
-        html.Td(str(today)),
-        html.Td(
-            str(iog_days),
-            style={
-                "color":"red",
-                "fontSize":"30px",
-                "fontWeight":"bold"
-            }
-        )
-    ])
-],
-style={
-    "width":"100%",
-    "textAlign":"center",
-    "border":"1px solid black"
-})
 # =============================================================================
-# 1. Load data
+# Load data
 # =============================================================================
 df = pd.read_excel("progress.xlsx")
 df["Date"] = pd.to_datetime(df["Date"])
-
+df["Start"] = df["Date"] - pd.Timedelta(hours=12)
+df["End"] = df["Date"] + pd.Timedelta(hours=12)
 # =============================================================================
-# 2. Task / Category list
+# Task / Category list
 # =============================================================================
 task_list = df["Task"].unique()
 cat_list = df["Category"].unique()
+#==============================================================================
+#Summary Table
+#==============================================================================
+projects = [
+    {
+        "name":"BeeX",
+        "logo":"beex_logo.png",
+        "start":datetime(2026,4,6).date()
+    },
+    {
+        "name":"IOG",
+        "logo":"iog_logo.png",
+        "start":datetime(2026,4,18).date()
+    }
+]
 
+def build_summary_table():
+    today = datetime.today().date()
+    rows = []
+    for p in projects:
+        days = (today - p["start"]).days
+        rows.append(
+            html.Tr([
+                html.Td(
+                    html.Img(
+                        src=get_asset_url(p["logo"]),
+                        style={"height":"40px"}
+                    )
+                ),
+                html.Td(p["name"]),
+                html.Td(str(p["start"])),
+                html.Td(str(today)),
+                html.Td(
+                    str(days),
+                    style={
+                        "color":"red",
+                        "fontSize":"30px",
+                        "fontWeight":"bold"
+                    }
+                )
+            ])
+        )
+    return html.Table(
+        [
+            html.Tr([
+                html.Th("Logo",style={"width":"20%"}),
+                html.Th("Company",style={"width":"20%"}),
+                html.Th("Start",style={"width":"20%"}),
+                html.Th("Today",style={"width":"20%"}),
+                html.Th("Days",style={"width":"20%"})
+            ])
+        ] + rows,
+        style={
+            "width":"100%",
+            "tableLayout":"fixed",
+            "textAlign":"center",
+            "border":"1px solid black",
+            "borderCollapse":"collapse"
+        }
+    )
 # =============================================================================
-# 3. App
+#KPI Dashboard
+# =============================================================================
+inspection_days = len(
+    df[df["Category"]=="Inspection"]
+)
+data_days = len(
+    df[df["Category"]=="Data Processing"]
+)
+wow_days = len(
+    df[df["Category"]=="WOW"]
+)
+off_days = len(
+    df[df["Category"]=="Day off"]
+)
+
+kpi_data = {
+    "Inspection Days": inspection_days,
+    "Data Processing Days": data_days,
+    "WOW Days": wow_days,
+    "Day Off Days": off_days
+}
+
+def build_card(title, value):
+    return html.Div([
+    html.H2(
+        value,
+        style={
+            "margin":"0",
+            "fontSize":"36px",
+            "color":"#1f77b4"
+        }
+    ),
+    html.P(
+        title,
+        style={
+            "margin":"5px 0 0 0"
+        }
+    )
+    ],
+    style={
+        "border":"1px solid lightgray",
+        "padding":"15px",
+        "width":"200px",
+        "display":"inline-block",
+        "margin":"10px",
+        "textAlign":"center"
+    })
+# =============================================================================
+# App
 # =============================================================================
 app = Dash(__name__)
-
 app.layout = html.Div([
     ##標題
     html.H2("🌊S2603BEX50 F2 Offshore Wind Farm Underwater Inspection"),
-    summary_table,
+    ##Summery Table
+    build_summary_table(),
+    ##KPI Card
+    html.Div(
+    [
+        build_card(title, value)
+        for title, value in kpi_data.items()
+    ],
+    style={
+        "display":"flex",
+        "justifyContent":"center",
+        "flexWrap":"wrap"
+    }
+    ),
     html.Br(),
     ##下拉選單
     html.Div([
@@ -116,7 +181,7 @@ app.layout = html.Div([
 ])
 
 # =============================================================================
-# 4. Color map
+# Color map
 # =============================================================================
 color_map = {
     "Inspection": "#1f77b4",         #藍
@@ -127,7 +192,7 @@ color_map = {
 }
 
 # =============================================================================
-# 5. Callback (dynamic update)
+# Callback (dynamic update)
 # =============================================================================
 @app.callback(
     Output("gantt-chart", "figure"),
@@ -139,10 +204,7 @@ def update_chart(selected_tasks, selected_cats):
     filtered = df[
         (df["Task"].isin(selected_tasks)) &
         (df["Category"].isin(selected_cats))
-    ]
-
-    filtered["Start"] = filtered["Date"] - pd.Timedelta(hours=12)
-    filtered["End"] = filtered["Date"] + pd.Timedelta(hours=12)
+    ].copy()
     
     cols = [
     "Date","Category","Task","Cluster",
@@ -151,6 +213,11 @@ def update_chart(selected_tasks, selected_cats):
     ]
     filtered[cols] = filtered[cols].fillna("")
     
+    filtered["Date_str"] = (
+    filtered["Date"]
+    .dt.strftime("%Y-%m-%d (%a)")
+    )
+    
     fig = px.timeline(
         filtered,
         x_start="Start",
@@ -158,7 +225,7 @@ def update_chart(selected_tasks, selected_cats):
         y="Task",
         color="Category",
         color_discrete_map=color_map,
-        custom_data=["Date", 
+        custom_data=["Date_str", 
                     "Category", 
                     "Task",
                     "Cluster",
@@ -185,19 +252,52 @@ def update_chart(selected_tasks, selected_cats):
     "Remark: %{customdata[8]}<extra></extra>"
     )
     
-    
-
     fig.update_yaxes(autorange="reversed")
-
+    ##today line
+    today_line = pd.Timestamp.now()
+    fig.add_shape(
+    type="line",
+    x0=today_line,
+    x1=today_line,
+    y0=0,
+    y1=1,
+    yref="paper",
+    line=dict(
+        color="red",
+        width=3,
+        dash="dash"
+    )
+    )
+    fig.add_annotation(
+    x=today_line,
+    y=1.03,
+    yref="paper",
+    text="Today",
+    showarrow=False,
+    font=dict(
+        size=14,
+        color="red"
+    )
+    )
+    ##chart height control
+    task_count = filtered["Task"].nunique()
+    chart_height = min(
+    max(650, task_count * 40),
+    1200
+    )
+    
     fig.update_layout(
         title="Offshore Gantt Chart",
-        height=650
+        height=chart_height,
+        xaxis=dict(
+            rangeslider=dict(
+                visible=True
+            )
+        )        
     )
-
     return fig
-
 # =============================================================================
-# 6. Run server
+# Run server
 # =============================================================================
 server = app.server
 
